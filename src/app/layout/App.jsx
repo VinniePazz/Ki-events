df1fd7780405ff1474e5f827c8a747b1a74543c4
Dima Smakouz <dimasmakouz@gmail.com>
2019-01-18 00:10:26 +0200

    End of start point

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-				<h1>Everything is still working!</h1>
-      </div>
+      <>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
-if (module.hot) {
-	module.hot.accept();
-}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-
+if (module.hot) {
+	module.hot.accept();
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
