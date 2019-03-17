94926e012269199980ea84681ca13228cc7a3f48
Dima Smakouz <dimasmakouz@gmail.com>
2019-03-17 22:17:40 +0200

    adding thunk, notafications nad mocking data api

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from 'redux-form'
+import {reducer as toastrReducer} from 'react-redux-toastr';
+
 import eventReducer from "../../features/event/eventReducer";
 
 const rootReducer = combineReducers({
 	form: formReducer,
-  events: eventReducer
+	events: eventReducer,
+	toastr: toastrReducer
 });
 
 export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import ReduxToastr from "react-redux-toastr";
+
 import "semantic-ui-css/semantic.min.css";
+import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
 import "./index.css";
 
-import { configureStore } from './app/store/configureStore';
-import ScrollToTop from './app/common/utils/ScrollToTop';
+import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/common/utils/ScrollToTop";
 import App from "./app/layout/App";
 
 const store = configureStore();
@@ -16,16 +19,21 @@ let render = () => {
     <Provider store={store}>
       <BrowserRouter>
         <ScrollToTop>
+          <ReduxToastr
+            position="bottom-right"
+            transitionIn="fadeIn"
+            transitionOut="fadeOut"
+          />
           <App />
         </ScrollToTop>
       </BrowserRouter>
     </Provider>,
-    document.getElementById('root')
+    document.getElementById("root")
   );
 };
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', () => {
+  module.hot.accept("./app/layout/App", () => {
     setTimeout(render);
   });
 }
