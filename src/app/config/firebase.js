5254947ff96804bf771f63a45f4ba00704cd0304
Dima Smakouz <dimasmakouz@gmail.com>
2019-03-20 00:30:04 +0200

    added photoPage and upploading functionallity to firebase storage

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,6 +1,7 @@
-import firebase from "firebase/app";
-import 'firebase/firestore';
-import 'firebase/auth';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBTpGxOnqKWpX7Y0Km-LUIzWLDL1NP4G30",
