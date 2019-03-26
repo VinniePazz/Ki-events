1d17db525cafa7bbfe118e243e6f6a523aad6559
Dima Smakouz <dimasmakouz@gmail.com>
2019-03-26 22:45:04 +0200

    This is Semantic Ui version! I wanna try Material Ui, cause responsive design with semantic is hell

--- src/features/user/PeopleDashboard/PeopleDashboard.jsx
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import { Grid, Segment, Header, Card } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { firestoreConnect } from 'react-redux-firebase'
-import PersonCard from './PersonCard';
+import React from "react";
+import { Grid, Segment, Header, Card } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
+import PersonCard from "./PersonCard";
 
-const query = ({auth}) => {
+const query = ({ auth }) => {
   return [
     {
-      collection: 'users',
+      collection: "users",
       doc: auth.uid,
-      subcollections: [{collection: 'following'}],
-      storeAs: 'following'
+      subcollections: [{ collection: "following" }],
+      storeAs: "following"
     },
     {
-      collection: 'users',
+      collection: "users",
       doc: auth.uid,
-      subcollections: [{collection: 'followers'}],
-      storeAs: 'followers'
+      subcollections: [{ collection: "followers" }],
+      storeAs: "followers"
     }
-  ]
-}
+  ];
+};
 
 const mapState = state => ({
   followings: state.firestore.ordered.following,
   followers: state.firestore.ordered.followers,
   auth: state.firebase.auth
-})
+});
 
-const PeopleDashboard = ({followings, followers}) => {
+const PeopleDashboard = ({ followings, followers }) => {
   return (
     <Grid>
       <Grid.Column width={16}>
@@ -49,4 +49,7 @@ const PeopleDashboard = ({followings, followers}) => {
   );
 };
 
-export default compose(connect(mapState), firestoreConnect(props => query(props)))(PeopleDashboard);
+export default compose(
+  connect(mapState),
+  firestoreConnect(props => query(props))
+)(PeopleDashboard);
